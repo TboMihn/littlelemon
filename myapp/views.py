f27a58382b3a9a59712ec9388fde7076d55483bd
tlfinn1618 <tlfinn1618@gmail.com>
2024-02-22 21:24:14 -0800

    Created the views for home, menu, about and book along with the paths to each view in a app level urls.py file.

--- myapp/views.py
+++ myapp/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse("<h1>Welcome to LittleLemon</h1>")
+
+def menu(request):
+    return HttpResponse("Menu")
+
+def about(request):
+    return HttpResponse("About Us")
+
+def book(request):
+    return HttpResponse("Book Your Reservations Today")
