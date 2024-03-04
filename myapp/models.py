25cf0e2fbc5df1d973cea05d0bd834534f9e6101
tlfinn1618 <tlfinn1618@gmail.com>
2024-03-03 22:08:51 -0800

    Added a few things. Created a forms.py file for creating forms. Created models for forms

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from unicodedata import name
 
 # Create your models here.
+# Menu Category
+# Menu
+
+class MenuCategory(models.Model):
+    menu_category_name = models.CharField(max_length=200)
+    
+class Menu(models.Model):
+    menu_item = models.CharField(max_length=200)
+    price = models.IntegerField(null=False)
+    category_id = models.ForeignKey(MenuCategory, on_delete=models.PROTECT, default=None)
+    
+    
+class Logger(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    time_log = models.TimeField()
